cbb6bdc9a0e3ab6d3525ee7863759b16645178d5
luizalocha <luiza.locha@souunilavras.com>
2025-04-04 20:05:15 -0300

    4

--- index.php
+++ index.php
@@ -15,21 +15,24 @@
             height: 100vh;
             margin: 0;
             text-align: center;
+            flex-direction: column; 
         }
 
         h2{
             font-size: 22px;
         }
+
+        img {
+            width: 100px; 
+            margin-top: 20px;
+        }
     </style>
 </head>
 <body>
     <div>
-        
-    <h1>Olá, Frank!</h1>
-    <h2>Essa é minha aplicação em PHP, instalada nos servidores da Azure na Índia!<h2>
-
-
-
-</div>
+        <h1>Olá, Frank!</h1>
+        <h2>Essa é minha aplicação em PHP, instalada nos servidores da Azure na Índia!</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg">
+    </div>
 </body>
 </html>

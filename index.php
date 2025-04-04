2fd770fe6cf76c13e728322483d7c892c40a0bd7
luizalocha <luiza.locha@souunilavras.com>
2025-04-04 20:13:08 -0300

    elefante

--- index.php
+++ index.php
@@ -15,16 +15,25 @@
             height: 100vh;
             margin: 0;
             text-align: center;
-            flex-direction: column; 
+            flex-direction: column;
         }
 
-        h2{
+        h2 {
             font-size: 22px;
         }
 
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .image {
+            margin-left: 20px;
+        }
+
         img {
-            width: 100px; 
-            margin-top: 20px;
+            width: 100px;
         }
     </style>
 </head>
@@ -32,7 +41,14 @@
     <div>
         <h1>Olá, Frank!</h1>
         <h2>Essa é minha aplicação em PHP, instalada nos servidores da Azure na Índia!</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg">
+        <div class="container">
+            <!-- Imagem da bandeira da Índia -->
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg" alt="Bandeira da Índia">
+            <!-- GIF do elefante indiano -->
+            <div class="image">
+                <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTc5dXIxaHkzb2hyNGZsMTIyZWdhaDN4OXR6b2x2d3JvZGlkdDNsciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TKUmFOx26jkbrPD7me/giphy.gif" alt="Elefante Indiano">
+            </div>
+        </div>
     </div>
 </body>
 </html>
